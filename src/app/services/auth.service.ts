1fcecc601cc8cbf752d4acae552c85860ddfb9bd
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-11-01 22:43:27 +0700

    update in auth service-create verify user is admin

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,9 +6,16 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class AuthService {
   public loggedInSubject = new BehaviorSubject<boolean>(false);
+  public isAdminSubject = new BehaviorSubject<boolean>(false);
   constructor() { }
 
   public sendStatusLoggedIn(isLoggedIn: boolean) {
     this.loggedInSubject.next(isLoggedIn);
   }
+
+  public sendIsAdmin(isAdmin: boolean) {
+    this.isAdminSubject.next(isAdmin);
+  }
+
+
 }
