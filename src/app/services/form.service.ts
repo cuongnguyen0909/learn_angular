d92ca9bc7201cc14d84cf05ab4dc5cdb07dae867
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-11-01 22:44:03 +0700

    send data from login form to display name on header componet

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
   public formMesageSubject = new BehaviorSubject<any>(null);
+  public dataSubject = new BehaviorSubject<User>({
+    id: 0, // provide an appropriate id value
+    username: '', // provide an appropriate username value
+    password: '', // provide an appropriate password value
+    email: ''
+  });
   constructor() { }
 
   public sendMesage(message: string) {
     this.formMesageSubject.next(message);
   }
+
+  public sendData(user: User) {
+    this.dataSubject.next(user);
+  }
+
+
 }
